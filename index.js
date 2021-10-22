9060e2d353675c06e55c80357c4b3b434aeee437
Soumikbhatta <soumikbhatta@gmail.com>
2021-10-22 12:46:43 +0530

    fix

--- index.js
+++ index.js
@@ -4,8 +4,6 @@ const request = require('request-promise');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-
-
 const generateScraperUrl = (apiKey) => `http://api.scraperapi.com?api_key=${apiKey}&autoparse=true`
 
 app.use(express.json());
